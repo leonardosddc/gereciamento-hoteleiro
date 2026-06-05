59ac6b9c49235888173d7ee625b691216e58d388
Leonardo Siqueira <leonardosiqueira132@gmail.com>
2026-06-04 22:04:22 -0300

    chore: trocar o nome do /admin

--- gerenciamento_hoteleiro/urls.py
+++ gerenciamento_hoteleiro/urls.py
@@ -3,7 +3,7 @@ from django.urls import path, include
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),  # O correto é .urls e não .back
+    path('acesso-gerencial-hotel/', admin.site.urls),  # O correto é .urls e não .back
     path('login/', auth_views.LoginView.as_view(template_name='autenticacao/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('', include('hotel.urls')),
